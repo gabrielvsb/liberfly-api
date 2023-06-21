785709da84a81365af4126a5ff3a60caa9fe109b
gabrielvsb <gabriel.vitorsb323@gmail.com>
2023-06-20 23:46:38 -0300

    creating routes from AuthController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\{
+    AuthController,
+    CarsController
+};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +18,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('cars', [CarsController::class, 'getAllCars']);
 
-Route::get('cars', [\App\Http\Controllers\CarsController::class, 'getAllCars']);
+Route::middleware(['api'])->prefix('auth')->group(function() {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+});
